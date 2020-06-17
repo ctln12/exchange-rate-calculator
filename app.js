61e9f3892df1087ae5387b238bb3482b35061924
Cataline Baert <snuf_kin@yahoo.com>
2020-06-17 21:34:03 +0200

    add second currency amount update on first currency amount change

--- app.js
+++ app.js
@@ -15,29 +15,26 @@ fetch('https://open.exchangerate-api.com/v6/latest')
     secondCurrency.insertAdjacentHTML('beforeend', `<option value='${currency}'>${currency}</option>`);
   });
   secondCurrency.value = 'EUR';
-  firstAmount.value = data.rates[currenciesList.shift()];
-  secondAmount.value = data.rates[secondCurrency.value];
+  firstAmount.value = data.rates[currenciesList.shift()].toFixed(2);
+  secondAmount.value = data.rates[secondCurrency.value].toFixed(2);
 })
 
-// Currency changes
-firstCurrency.addEventListener('change', (event) => { compute(event.target, secondCurrency) });
-secondCurrency.addEventListener('change', (event) => { compute(firstCurrency, event.target) });
-
-// Amount changes
-
 // Compute function
-const compute = (firstCurrency, secondCurrency) => {
+const compute = () => {
   fetch(`https://api.exchangerate-api.com/v4/latest/${firstCurrency.value}`)
-  .then(res => res.json())
-  .then(data => {
-    // console.log(Object.keys(data.rates));
-    const exchangeRate = data.rates[secondCurrency.value]
-    console.log(exchangeRate);
-    const firstAmount = document.getElementById('first-amount');
-    const secondAmount = document.getElementById('second-amount');
-    secondAmount.value = firstAmount.value * exchangeRate;
-    console.log(`${firstAmount.value} ${firstCurrency.value} = ${secondAmount.value} ${secondCurrency.value}`);
-  })
+    .then(res => res.json())
+    .then(data => {
+      const exchangeRate = data.rates[secondCurrency.value]
+      console.log(exchangeRate);
+      secondAmount.value = (firstAmount.value * exchangeRate).toFixed(2);
+      console.log(`${firstAmount.value} ${firstCurrency.value} = ${secondAmount.value} ${secondCurrency.value}`);
+    })
 }
 
-console.log("Goodbye from the console!");
+// Currency changes
+firstCurrency.addEventListener('change', compute);
+secondCurrency.addEventListener('change', compute);
+
+// Amount changes
+firstAmount.addEventListener('input', compute);
+secondAmount.addEventListener('input', compute);
