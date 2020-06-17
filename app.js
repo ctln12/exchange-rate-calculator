c47d0f20cad8072dd5dcf578e68f5e01095d9a9e
Cataline Baert <snuf_kin@yahoo.com>
2020-06-17 20:47:59 +0200

    add exchange rate update when changing currencies

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 console.log("Hello from the console!");
 
 // Initializing
-const firstCurrencySelect = document.getElementById('first-currency');
-const secondCurrencySelect = document.getElementById('second-currency');
-
-const firstCurrencyAmount = document.getElementById('first-amount');
-firstCurrencyAmount.value = 1;
+const firstCurrency = document.getElementById('first-currency');
+const secondCurrency = document.getElementById('second-currency');
+const firstAmount = document.getElementById('first-amount');
+const secondAmount = document.getElementById('second-amount');
 
 fetch('https://open.exchangerate-api.com/v6/latest')
 .then(res => res.json())
 .then(data => {
   const currenciesList = Object.keys(data.rates);
   currenciesList.forEach(currency => {
-    firstCurrencySelect.insertAdjacentHTML('beforeend', `<option value='${currency}'>${currency}</option>`);
+    firstCurrency.insertAdjacentHTML('beforeend', `<option value='${currency}'>${currency}</option>`);
+    secondCurrency.insertAdjacentHTML('beforeend', `<option value='${currency}'>${currency}</option>`);
   });
-  currenciesList.sort().forEach(currency => {
-    secondCurrencySelect.insertAdjacentHTML('beforeend', `<option value='${currency}'>${currency}</option>`);
-  });
-  const secondCurrencyAmount = document.getElementById('second-amount');
-  secondCurrencyAmount.value = data.rates[currenciesList.sort().shift()];
+  secondCurrency.value = 'EUR';
+  firstAmount.value = data.rates[currenciesList.shift()];
+  secondAmount.value = data.rates[secondCurrency.value];
 })
 
 // Currency changes
-const firstCurrency = document.getElementById('first-currency').value;
-const secondCurrency = document.getElementById('first-currency').value;
+firstCurrency.addEventListener('change', (event) => { compute(event.target, secondCurrency) });
+secondCurrency.addEventListener('change', (event) => { compute(firstCurrency, event.target) });
 
 // Amount changes
 
 // Compute function
-const compute = () => {
-  fetch(`https://api.exchangerate-api.com/v4/latest/${firstCurrency}`)
+const compute = (firstCurrency, secondCurrency) => {
+  fetch(`https://api.exchangerate-api.com/v4/latest/${firstCurrency.value}`)
   .then(res => res.json())
   .then(data => {
-    console.log(Object.keys(data.rates));
-    const exchangeRate = data.rates[secondCurrency]
+    // console.log(Object.keys(data.rates));
+    const exchangeRate = data.rates[secondCurrency.value]
     console.log(exchangeRate);
-    const secondCurrencyAmount = firstCurrencyAmount * exchangeRate;
-    console.log(`${firstCurrencyAmount} ${firstCurrency} = ${secondCurrencyAmount} ${secondCurrency}`);
+    const firstAmount = document.getElementById('first-amount');
+    const secondAmount = document.getElementById('second-amount');
+    secondAmount.value = firstAmount.value * exchangeRate;
+    console.log(`${firstAmount.value} ${firstCurrency.value} = ${secondAmount.value} ${secondCurrency.value}`);
   })
 }
 
